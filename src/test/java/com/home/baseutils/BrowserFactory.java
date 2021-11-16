5800fcaa1b18ba167d904a37773e98867901624c
yadukrishnan.d <yadukrishnan.d@gmail.com>
2021-11-15 23:45:32 -0500

    chrome options added in factory class

--- src/test/java/com/home/baseutils/BrowserFactory.java
+++ src/test/java/com/home/baseutils/BrowserFactory.java
@@ -3,8 +3,10 @@ package com.home.baseutils;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
 
 import java.util.concurrent.TimeUnit;
 
@@ -25,12 +27,21 @@ public class BrowserFactory {
                 break;
             default:
                 WebDriverManager.chromedriver().setup();
-                driver = new ChromeDriver();
+                driver = new ChromeDriver(getChromeOptions());
                 break;
         }
         driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
         driver.manage().window().maximize();
         return driver;
 
+    }
+
+    private ChromeOptions getChromeOptions() {
+        ChromeOptions options = new ChromeOptions();
+        options.setHeadless(Boolean.parseBoolean(System.getProperty("headless")));
+        options.addArguments("disable-popup-blocking");
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+        capabilities.setAcceptInsecureCerts(Boolean.parseBoolean(System.getProperty("insecure_cert")));
+        return options.merge(capabilities);
     }
 }
