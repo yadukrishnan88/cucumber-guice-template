079476df972d51279974272d1e1ce134cd575023
yadukrishnan.d <yadukrishnan.d@gmail.com>
2021-11-27 01:04:14 -0500

    AssertJ Core added

--- src/test/java/com/home/steps/DemoBlazeSteps.java
+++ src/test/java/com/home/steps/DemoBlazeSteps.java
@@ -8,7 +8,8 @@ import io.cucumber.java.en.And;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
-import org.testng.Assert;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 public class DemoBlazeSteps {
 
@@ -38,13 +39,13 @@ public class DemoBlazeSteps {
     public void iShouldSeeMessage(String text, String userName) {
         String welcomeText = demoBlazeHomePage.getWelcomeText();
         String expectedText = text + " " + userName;
-        Assert.assertEquals(welcomeText.toLowerCase(), expectedText.toLowerCase(), "Welcome Label Mismatches");
+        assertThat(welcomeText).as("Welcome Label Mismatches").isEqualTo(expectedText);
         Log.info("Captured the welcome text \"" + welcomeText + "\" from the page");
     }
 
     @And("the page title should be {string}")
     public void thePageTitleShouldBe(String pageTitle) {
-        Assert.assertEquals(driverFactory.driver.getTitle(), pageTitle);
+        assertThat(driverFactory.driver.getTitle()).as("Page Title Mismatches").isEqualTo(pageTitle);
         Log.info("Page title is: " + driverFactory.driver.getTitle());
     }
 }
